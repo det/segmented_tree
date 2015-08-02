2aa4a85673fdfcfbafb816a467153f4256c8865a
Chris Clearwater <chris@detrino.org>
2015-08-01 23:15:09 -0700

    Add boost license

--- include/boost/container/segmented_tree_seq.hpp
+++ include/boost/container/segmented_tree_seq.hpp
@@ -1,3 +1,8 @@
+//          Copyright Chris Clearwater 2014 - 2015.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <array>
